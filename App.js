7c7309693dacf7ab277742ee6fc6b6d5a643aebc
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-12-21 14:03:33 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import AppNavigator from './navigator/AppNavigator';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { URI_CONTENTFUL, BEARER_KEY } from '@env';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const client = new ApolloClient({
+  uri: `${URI_CONTENTFUL}`,
+  credentials: "same-origin",
+  headers: {
+    Authorization: `Bearer ${BEARER_KEY}`
+  }
 });
+
+const initialState = {
+  action: ''
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'OPEN_MENU':
+      return { action: 'openMenu' };
+    case 'CLOSE_MENU':
+      return { action: 'closeMenu' };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  </ApolloProvider>
+);
+
+export default App;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,354 @@
+import React from 'react';
+import { ScrollView, SafeAreaView, TouchableOpacity, Animated, Easing, StatusBar, Platform } from 'react-native';
+import styled from 'styled-components';
+import Card from '../components/Card';
+import Course from '../components/Course';
+// import { Ionicons } from '@expo/vector-icons';
+import { NotificationIcon } from '../components/Icons';
+import Logo from '../components/Logo';
+import Menu from '../components/Menu';
+import { connect } from "react-redux";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+
+const CardsQuery = gql`
+      {
+        cardsCollection {
+          items {
+            title
+            subtitle
+            image {
+              title
+              description
+              contentType
+              fileName
+              size
+              url
+              width
+              height
+            }
+            subtitle
+            caption
+            logo {
+              title
+              description
+              contentType
+              fileName
+              size
+              url
+              width
+              height
+            }
+            content
+          }
+        }
+      }
+    `;
+
+function mapStateToProps(state) {
+  return { action: state.action };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    openMenu: () =>
+      dispatch({
+        type: 'OPEN_MENU'
+      })
+  };
+}
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    headerShown: false
+  };
+
+  state = {
+    scale: new Animated.Value(1),
+    opacity: new Animated.Value(1)
+  };
+
+  componentDidMount() {
+    StatusBar.setBarStyle('dark-content', true);
+
+    if (Platform.OS === "android") StatusBar.setBarStyle("light-content", true);
+  }
+
+  componentDidUpdate() {
+    this.toggleMenu();
+  }
+
+  toggleMenu = () => {
+    if (this.props.action === 'openMenu') {
+      Animated.timing(this.state.scale, {
+        toValue: 0.9,
+        duration: 300,
+        easing: Easing.in()
+      }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 0.5
+      }).start();
+
+      StatusBar.setBarStyle('light-content', true);
+    }
+
+    if (this.props.action == 'closeMenu') {
+      Animated.timing(this.state.scale, {
+        toValue: 1,
+        duration: 300,
+        easing: Easing.in()
+      }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 1
+      }).start();
+
+      StatusBar.setBarStyle('dark-content', true);
+    }
+  };
+
+  render() {
+
+    return (
+      <RootView>
+        <Menu />
+        <AnimatedContainer style={{ transform: [{ scale: this.state.scale }], opacity: this.state.opacity }}>
+          <SafeAreaView>
+            <ScrollView style={{ height: '100%' }}>
+              <TitleBar>
+                <TouchableOpacity onPress={this.props.openMenu} style={{ position: 'absolute', top: 0, left: 20 }}>
+                  <Avatar source={require('../assets/avatar-default.jpg')} />
+                </TouchableOpacity>
+                <Title>Welcome to React native</Title>
+                <Name>Lulu</Name>
+                <NotificationIcon style={{ position: "absolute", right: 20, top: 5 }} />
+
+              </TitleBar>
+              <ScrollView style={{ flexDirection: 'row', padding: 20, paddingLeft: 12, paddingTop: 30 }} horizontal={true} showsHorizontalScrollIndicator={false}>
+                {logos.map((logo, index) => (
+                  <Logo key={index} image={logo.image} text={logo.text} />
+                ))}
+
+              </ScrollView>
+              <Subtitle>{"Continue Learning".toUpperCase()}</Subtitle>
+              <ScrollView horizontal={true} style={{ paddingBottom: 30 }} showsHorizontalScrollIndicator={false}>
+                <Query query={CardsQuery}>
+                  {({ loading, error, data }) => {
+                    if (loading) return <Message>Loading...</Message>;
+                    if (error) return <Message>Error...</Message>;
+
+                    return (
+                      <CardsContainer>
+                        {data.cardsCollection.items.map((card, index) => (
+                          <TouchableOpacity
+                            key={index}
+                            onPress={() => {
+                              this.props.navigation.push("Section", {
+                                section: card
+                              });
+                            }}>
+                            <Card
+                              key={index}
+                              title={card.title}
+                              image={{ uri: card.image.url }}
+                              caption={card.caption}
+                              logo={{ uri: card.logo.url }}
+                              subtitle={card.subtitle}
+                              content={card.content}
+                            />
+                          </TouchableOpacity>
+                        ))}
+                      </CardsContainer>
+                    );
+                  }}
+                </Query>
+
+              </ScrollView>
+              <Subtitle>{"Popular Courses".toUpperCase()}</Subtitle>
+              <CoursesContainer>
+                {courses.map((course, index) => (
+                  <Course key={index}
+                    title={course.title}
+                    subtitle={course.subtitle}
+                    image={course.image}
+                    logo={course.logo}
+                    author={course.author}
+                    avatar={course.avatar}
+                    caption={course.caption} />
+                ))}
+              </CoursesContainer>
+            </ScrollView>
+          </SafeAreaView>
+        </AnimatedContainer>
+      </RootView>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
+
+const RootView = styled.View`
+  background: black;
+  flex: 1;
+`;
+
+const Subtitle = styled.Text`
+color: #b8bece;
+font-weight:600;
+font-size: 15px;
+margin-left: 20px;
+margin-top: 20px;
+text-transform: uppercase;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+
+const Title = styled.Text`
+font-size: 16px;
+color: #b8bece;
+font-weight: 500;
+`;
+
+const Name = styled.Text`
+font-size: 20px;
+color: #3c4560;
+font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+width:100%;
+margin-top: 50px;
+padding-left: 80px; 
+`;
+
+const Avatar = styled.Image`
+width: 44px;
+height: 44px;
+background: black;
+border-radius: 22px;
+`;
+
+const Message = styled.Text`
+margin: 20px;
+color: #b8bece;
+font-size: 15px;
+font-weight: 500;
+`;
+
+const CardsContainer = styled.View`
+flex-direction: row;
+padding-left: 10px;
+
+`;
+
+const CoursesContainer = styled.View`
+flex-direction: row;
+flex-wrap: wrap;
+padding-left: 10px;
+`;
+
+
+const logos = [
+  {
+    image: require('../assets/logo-framerx.png'),
+    text: 'Framer X'
+  },
+  {
+    image: require('../assets/logo-figma.png'),
+    text: 'Figma'
+  },
+  {
+    image: require('../assets/logo-studio.png'),
+    text: 'Studio'
+  },
+  {
+    image: require('../assets/logo-react.png'),
+    text: 'React'
+  },
+  {
+    image: require('../assets/logo-swift.png'),
+    text: 'Swift'
+  },
+  {
+    image: require('../assets/logo-sketch.png'),
+    text: 'Sketch'
+  }
+];
+
+const cards = [
+  {
+    title: 'React Native for Designers',
+    image: require('../assets/background11.jpg'),
+    subtitle: 'React Native',
+    caption: '1 of 12 sections',
+    logo: require('../assets/logo-react.png')
+  },
+  {
+    title: 'Styled Components',
+    image: require('../assets/background12.jpg'),
+    subtitle: 'React Native',
+    caption: '2 of 12 sections',
+    logo: require('../assets/logo-react.png')
+  },
+  {
+    title: 'Props and Icons',
+    image: require('../assets/background13.jpg'),
+    subtitle: 'React Native',
+    caption: '3 of 12 sections',
+    logo: require('../assets/logo-react.png')
+  },
+  {
+    title: 'Static Data and Loop',
+    image: require('../assets/background14.jpg'),
+    subtitle: 'React Native',
+    caption: '4 of 12 sections',
+    logo: require('../assets/logo-react.png')
+  }
+];
+
+const courses = [
+  {
+    title: "Prototype in InVision Studio",
+    subtitle: "10 sections",
+    image: require("../assets/background13.jpg"),
+    logo: require("../assets/logo-studio.png"),
+    author: "Meng To",
+    avatar: require("../assets/avatar.jpg"),
+    caption: "Design and interactive prototype"
+  },
+  {
+    title: "React for Designers",
+    subtitle: "12 sections",
+    image: require("../assets/background11.jpg"),
+    logo: require("../assets/logo-react.png"),
+    author: "Meng To",
+    avatar: require("../assets/avatar.jpg"),
+    caption: "Learn to design and code a React site"
+  },
+  {
+    title: "Design and Code with Framer X",
+    subtitle: "10 sections",
+    image: require("../assets/background14.jpg"),
+    logo: require("../assets/logo-framerx.png"),
+    author: "Meng To",
+    avatar: require("../assets/avatar.jpg"),
+    caption: "Create powerful design and code components for your app"
+  },
+  {
+    title: "Design System in Figma",
+    subtitle: "10 sections",
+    image: require("../assets/background6.jpg"),
+    logo: require("../assets/logo-figma.png"),
+    author: "Meng To",
+    avatar: require("../assets/avatar-default.jpg"),
+    caption:
+      "Complete guide to designing a site using a collaborative design tool"
+  }
+]
